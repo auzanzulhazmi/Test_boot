7e1d6d59dd0777bef8e43b1c6d0db3887c1bd8f9
auzanzulhazmi <60720604+auzanzulhazmi@users.noreply.github.com>
2021-09-29 22:59:36 +0700

    Update login_spec.js

--- cypress/integration/3-login-test/login_spec.js
+++ cypress/integration/3-login-test/login_spec.js
@@ -1,8 +1,8 @@
 describe('Cypress Demo-Login',() => {
     it('login scenario',() => {
         cy.visit('http://siakad.polinema.ac.id/index.php')
-        cy.get('#username').type('*******') // type your username
-        cy.get('#password').type('*******') // type password
+        cy.get('#username').type('*******')
+        cy.get('#password').type('*******')
         cy.get('.form-actions > .btn').click()
         cy.title().should('include','Sistem Informasi Akademik')
   
